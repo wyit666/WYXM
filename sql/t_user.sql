9f2cf7b6b0b1a3e1e0c656c955adfdcdead9efbf
wangyan <wy_svip@sina.cn>
2020-06-12 16:38:13 +0800

    sql文件提交

---
+++ sql/t_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `t_user` (`id`, `user_name`, `user_sex`, `user_identity`, `user_phone`, `user_code`, `user_password`) VALUES ('1', '小炎哥', '男', '12345678', '12345678', 'adminwy', 'adminwy');
